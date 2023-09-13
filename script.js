b639e18abf117ff47273401bad42d9ba07d43f28
cj <116382026+Krz-143@users.noreply.github.com>
2023-09-13 08:50:51 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+// Add JavaScript functionality here
+document.getElementById('watchVideo').addEventListener('click', function() {
+    // Replace 'video-url' with the actual YouTube video URL
+    window.open('https://www.youtube.com/watch?v=video-url', '_blank');
+});
+
+document.getElementById('buyNow').addEventListener('click', function() {
+    // Redirect to the online store for purchasing
+    window.location.href = 'https://example.com/airpods';
+});
